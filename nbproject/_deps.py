bb2a91717e48967d813e78c1cc4776139063dabc
Koncopd <koncopd@gmail.com>
2022-03-24 12:30:12 +0100

    func to parse nb dependencies

---
+++ nbproject/_deps.py
@@ -0,0 +1,64 @@
+from importlib_metadata import packages_distributions, version, PackageNotFoundError
+from nbformat import NotebookNode
+from ast import parse, walk, Import, ImportFrom
+from stdlib_list import stdlib_list
+
+# todo: infer proper python version for the libs from the notebook
+std_libs = set(stdlib_list())
+
+
+def cell_imports(cell_source):
+    # based on https://github.com/bndr/pipreqs
+    tree = parse(cell_source)
+    for node in walk(tree):
+        if isinstance(node, Import):
+            for subnode in node.names:
+                name = subnode.name.partition(".")[0]
+                if name != "":
+                    yield name
+        elif isinstance(node, ImportFrom):
+            name = node.module.partition(".")[0]
+            if name != "":
+                yield name
+
+
+def get_deps_nb(content, versions=False):
+    # parse the notebook content and infer all dependencies
+    if (
+        isinstance(content, NotebookNode) or isinstance(content, dict)
+    ) and "cells" in content:
+        cells = content["cells"]
+    elif isinstance(content, list) and len(content) > 0 and "cell_type" in content[0]:
+        cells = content
+    else:
+        raise ValueError("Invalid content - neither notebook nor cells.")
+
+    pkgs = set()
+
+    pkgs_dists = packages_distributions()
+
+    for cell in cells:
+        if cell["cell_type"] == "code":
+            cell_source = cell["source"]
+            if "import" not in cell_source:
+                continue
+            else:
+                for imp in cell_imports(cell_source):
+                    if imp in std_libs:
+                        continue
+                    if imp in pkgs_dists:
+                        pkgs.update(pkgs_dists[imp])
+                    else:
+                        pkgs.add(imp)
+
+    pkgs = list(pkgs)
+    if not versions:
+        return pkgs
+
+    versions = []
+    for pkg in pkgs:
+        try:
+            versions.append(version(pkg))
+        except PackageNotFoundError:
+            versions.append("")
+    return pkgs, versions
