16d01de4303e1a26dfaff34fe364203ff5c389b3
Alex Wolf <f.alexander.wolf@gmail.com>
2022-04-19 00:16:26 +0200

    :lipstick: changed to dict format, applied styling

--- nbproject/_header.py
+++ nbproject/_header.py
@@ -10,7 +10,7 @@ from enum import Enum
 from ._jupyter_communicate import notebook_path
 
 
-def nbproject_uuid():  # rename to nbproject_uid also in metadata slot?
+def nbproject_uid():  # rename to nbproject_uid also in metadata slot?
     """An 8-byte ID encoded as a 12-character base62 string."""
     # https://github.com/laminlabs/notes/blob/main/2022-04-04-human-friendly-ids.ipynb
     base62 = string.digits + string.ascii_letters.swapcase()
@@ -20,27 +20,24 @@ def nbproject_uuid():  # rename to nbproject_uid also in metadata slot?
 
 # schema within the metadata section
 class JSONSchema(BaseModel):
-    nbproject_uuid: str  # a full 32 digit uuid4.hex string
+    nbproject_uid: str  # a full 32 digit uuid4.hex string
     nbproject_time_init: datetime
-    nbproject_time_edit: datetime
 
 
 # user visible name & type configuration
 class UserSchema(BaseModel):
-    id: Union[
-        str, int
-    ] = nbproject_uuid  # the user only sees the first couple of digits
+    id: Union[str, int] = nbproject_uid  # the user only sees the first couple of digits
     time_init: Union[date, datetime]
-    time_edit: Union[date, datetime]
+    time_run: Union[date, datetime]  # not part of the ipynb metadata section
 
 
 # display configuration
 class DisplayConf(BaseModel):
-    id: int = 4  # number of digits visible, important for truncating uuid
-    time_init: Enum("choice", ["date", "datetime"]) = "date"  # noqa: F821
-    time_edit: Enum("choice", ["date", "datetime"]) = "date"  # noqa: F821
+    time_init: Enum("choice", ["date", "datetime"]) = "datetime"  # noqa: F821
+    time_run: Enum("choice", ["date", "datetime"]) = "datetime"  # noqa: F821
 
 
+# displays fields within the ipynb metadata section and on-the-fly computed
 class Display:
     def __init__(self, nb_metadata):
         self.metadata = nb_metadata
@@ -48,28 +45,31 @@ class Display:
 
     def id(self):
         """Shorten ID display."""
-        return self.metadata["nbproject_uuid"][: self.conf.id]
+        uid = self.metadata["nbproject"]["uid"]
+        return f"{uid[:4]}<span style='opacity:0.3'>{uid[4:]}"
 
     def time_init(self):
         """Shorten ID display."""
-        dt = datetime.fromisoformat(self.metadata["nbproject_time_init"])
+        dt = datetime.fromisoformat(self.metadata["nbproject"]["time_init"])
         if self.conf.time_init == "date":
             return dt.date()
         else:
-            return dt.isoformat()  # probably something more reduced is better
+            return dt.strftime(
+                "%Y-%m-%d %H:%M"
+            )  # probably something more reduced is better
 
-    def time_edit(self):
+    # this is not part of the ipynb metadata section
+    def time_run(self, dt: datetime):
         """Shorten ID display."""
-        dt = datetime.fromisoformat(self.metadata["nbproject_time_edit"])
-        if self.conf.time_edit == "date":
+        if self.conf.time_run == "date":
             return dt.date()
         else:
-            return dt.isoformat()  # probably something more reduced is better
+            return dt.strftime(
+                "%Y-%m-%d %H:%M"
+            )  # probably something more reduced is better
 
 
 class Header:
-    # filename should disappear as here but be auto-detected
-    # from the jupyter notebook that calls this
     def __init__(self, filepath=None):
         if filepath is None:
             filepath = notebook_path()
@@ -84,26 +84,27 @@ class Header:
             raise RuntimeError(
                 "try passing the filepath manually to nbproject.Header()"
             )
-        if "nbproject_uuid" not in nb.metadata:
+        # initialize
+        if "nbproject" not in nb.metadata:
             logger.info(
-                "to initialize nbproject: hit save, load notebook from disk ('revert')"
-                " & restart"
+                "To initialize nbproject for this notebook:\n* in Jupyter Lab: hit"
+                " save, load notebook from disk ('revert') & restart"
             )
-            # return string JSON-serializable string representation
-            # we *do* want UUID.hex as we don't need hyphens for user intuition
-            # user intuition comes through a shortened version of the hex string
-            nb.metadata["nbproject_uuid"] = nbproject_uuid()
-            nb.metadata["nbproject_time_init"] = datetime.now(timezone.utc).isoformat()
-            nb.metadata["nbproject_time_edit"] = datetime.now(timezone.utc).isoformat()
+            nb.metadata["nbproject"] = {}
+            nb.metadata["nbproject"]["uid"] = nbproject_uid()
+            nb.metadata["nbproject"]["time_init"] = datetime.now(
+                timezone.utc
+            ).isoformat()
             nbf.write(nb, filepath)
+        # read from ipynb metadata and add on-the-fly computed metadata
         else:
             display_ = Display(nb.metadata)
             df = pd.DataFrame(
                 {
-                    "id": [display_.id()],
+                    "uid": [display_.id()],
                     "time_init": [display_.time_init()],
-                    "time_edit": [display_.time_edit()],
+                    "time_run": [display_.time_run(datetime.now(timezone.utc))],
                 },
                 index=[" "],
             )
-            display(df.T)  # noqa
+            display(df.T.style)  # noqa
