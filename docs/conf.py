bf92aad676d1c1a9d1db2535f9114dc09c6eafa9
Alex Wolf <f.alexander.wolf@gmail.com>
2022-05-08 11:32:51 +0200

    📝 Migrate docs to Lamin theme & start building them again (#24)
    * :lipstick: added theme
    
    * :recycle: migrate from rst to md
    
    * :building_construction: update conf
    
    * :green_heart: add prettier
    
    * :recycle: run prettier on README
    
    * :construction_worker: update ci to deploy docs
    
    * :bug: fix slug
    
    * :bug: fix slug

--- docs/conf.py
+++ docs/conf.py
@@ -16,61 +16,69 @@ author = "Lamin Labs"
 copyright = f"{datetime.now():%Y}, {author}"
 release = nbproject.__version__.replace(".dirty", "")
 
-html_theme = "sphinx_book_theme"
-html_theme_options = {
-    "repository_url": "https://github.com/laminlabs/nbproject",
-    "use_repository_button": True,
-}
-source_suffix = [".rst", ".ipynb"]
-master_doc = "index"
-default_role = "literal"
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
-
-
 extensions = [
     "sphinx.ext.autodoc",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.doctest",
-    "sphinx.ext.coverage",
-    "sphinx.ext.mathjax",
     "sphinx.ext.napoleon",
     "sphinx.ext.autosummary",
-    "sphinx_autodoc_typehints",  # needs to be after napoleon
     "IPython.sphinxext.ipython_console_highlighting",  # noqa https://github.com/spatialaudio/nbsphinx/issues/24
     "myst_nb",
     *[p.stem for p in (HERE / "extensions").glob("*.py")],
 ]
 
+templates_path = ["lamin-sphinx-theme/_templates"]
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    "**.ipynb_checkpoints",
+    "README.md",
+    ".pytest_cache/*",
+]
 
-# Generate the API documentation when building
+html_theme = "pydata_sphinx_theme"
+
+html_theme_options = {
+    "show_prev_next": False,
+    "use_edit_page_button": False,  # currently unused
+    "search_bar_text": "Search",  # currently unused
+    "navbar_end": ["theme-switcher"],
+    "navbar_align": "left",
+    "footer_items": ["copyright"],
+}
+
+html_context = {
+    "default_mode": "auto",
+    "github_user": "laminlabs",
+    "github_repo": "nbproject",
+    "github_version": "main",
+}
+
+html_title = "nbproject | Lamin Labs"
+html_logo = "lamin-sphinx-theme/_static/img/logo.svg"
+html_favicon = "lamin-sphinx-theme/_static/img/favicon.ico"
+html_static_path = ["lamin-sphinx-theme/_static"]
+
+html_sidebars = {
+    "**": ["sidebar-nav-bs"],
+}
+
+source_suffix = [".md", ".ipynb"]
+master_doc = "index"
+default_role = "literal"
+
+# Generate the API documentation
 autosummary_generate = True
 autodoc_member_order = "bysource"
-# autodoc_default_flags = ['members']
-napoleon_google_docstring = False
-napoleon_numpy_docstring = True
+napoleon_google_docstring = True
+napoleon_numpy_docstring = False
 napoleon_include_init_with_doc = False
 napoleon_use_rtype = True  # having a separate entry generally helps readability
 napoleon_use_param = True
-napoleon_custom_sections = [("Params", "Parameters")]
 todo_include_todos = False
-nitpicky = True  # Report broken links
-nitpick_ignore = [
-    ("py:meth", "pandas.DataFrame.iloc"),
-    ("py:meth", "pandas.DataFrame.loc"),
-]
-suppress_warnings = ["ref.citation"]
+nitpicky = True  # report broken links
 
 
 def setup(app: Sphinx):
     # Don’t allow broken links. DO NOT CHANGE THIS LINE, fix problems instead.
     app.warningiserror = False
-
-
-intersphinx_mapping = dict(
-    pandas=("https://pandas.pydata.org/pandas-docs/stable/", None),
-    zarr=("https://zarr.readthedocs.io/en/stable/", None),
-    xarray=("http://xarray.pydata.org/en/stable/", None),
-)
-qualname_overrides = {
-    "anndata._core.anndata.AnnData": "anndata.AnnData",
-}
+    app.add_css_file("custom.css")
