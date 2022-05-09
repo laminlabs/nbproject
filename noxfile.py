1046c758250d894fae69050e4e03695e5393d3ec
Alex Wolf <f.alexander.wolf@gmail.com>
2022-05-10 01:19:18 +0200

    :construction_worker: move tests to nox

--- noxfile.py
+++ noxfile.py
@@ -11,6 +11,12 @@ def lint(session: nox.Session) -> None:
 
 
 @nox.session
+def test(session):
+    session.install(".[dev, test]")
+    session.run("pytest", "-s")  # write output instead of capturing it (more verbose)
+
+
+@nox.session
 def docs(session: nox.Session):
     session.install("-r", "docs/lamin_sphinx/requirements.txt")
     session.run(*"sphinx-build -b html source _build/html".split())
