7899acbcdb386684b8fd560e7a56eb57aff86665
Alex Wolf <f.alexander.wolf@gmail.com>
2022-05-10 02:03:38 +0200

    💚 Refactor and fix docs build, refactor CI (#27)
    * :recycle: fix double import of nbproject
    
    * 📝 Move guides to dedicated directory, fix admonition (#25)
    
    * :recycle: try fixing admonition
    
    * :memo: move notebooks to guides directory
    
    * :memo: update landing page
    
    * :recycle: renamed to lamin_sphinx
    
    * :recycle: update conf
    
    * :recycle: upgrade submodule
    
    * :construction_worker: build docs with nox
    
    * :construction_worker: move tests to nox
    
    * :green_heart: update configuration
    
    * :bug: add parent
    
    * :green_heart: install deps
    
    * :green_heart: do not exec notebooks
    
    * :green_heart: fix nox session
    
    * :memo: fix sidebar
    
    * :recycle: point submodule to main

--- docs/conf.py
+++ docs/conf.py
@@ -1,84 +1,20 @@
-from datetime import datetime
-import nbproject  # noqa
 import sys
 from pathlib import Path
-from sphinx.application import Sphinx
 
 HERE = Path(__file__).parent
-sys.path[:0] = [str(HERE.parent), str(HERE / "extensions")]
+sys.path[:0] = [str(HERE)]
+from lamin_sphinx import *  # noqa
 import nbproject  # noqa
 
 for generated in HERE.glob("nbproject.*.rst"):
     generated.unlink()
 
 project = "nbproject"
-author = "Lamin Labs"
-copyright = f"{datetime.now():%Y}, {author}"
-release = nbproject.__version__.replace(".dirty", "")
-
-extensions = [
-    "sphinx.ext.autodoc",
-    "sphinx.ext.napoleon",
-    "sphinx.ext.autosummary",
-    "IPython.sphinxext.ipython_console_highlighting",  # noqa https://github.com/spatialaudio/nbsphinx/issues/24
-    "myst_nb",
-    *[p.stem for p in (HERE / "extensions").glob("*.py")],
-]
-
-templates_path = ["lamin-sphinx-theme/_templates"]
-exclude_patterns = [
-    "_build",
-    "Thumbs.db",
-    ".DS_Store",
-    "**.ipynb_checkpoints",
-    "README.md",
-    ".pytest_cache/*",
-]
-
-html_theme = "pydata_sphinx_theme"
-
-html_theme_options = {
-    "show_prev_next": False,
-    "use_edit_page_button": False,  # currently unused
-    "search_bar_text": "Search",  # currently unused
-    "navbar_end": ["theme-switcher"],
-    "navbar_align": "left",
-    "footer_items": ["copyright"],
-}
-
-html_context = {
-    "default_mode": "auto",
-    "github_user": "laminlabs",
-    "github_repo": "nbproject",
-    "github_version": "main",
-}
-
-html_title = "nbproject | Lamin Labs"
-html_logo = "lamin-sphinx-theme/_static/img/logo.svg"
-html_favicon = "lamin-sphinx-theme/_static/img/favicon.ico"
-html_static_path = ["lamin-sphinx-theme/_static"]
-
+html_title = f"{nbproject} | Lamin Labs"
+release = nbproject.__version__
+html_context["github_repo"] = "nbproject"  # noqa
 html_sidebars = {
-    "**": ["sidebar-nav-bs"],
+    "*": [],
+    "guides": ["sidebar-nav-bs"],
+    "guides/*": ["sidebar-nav-bs"],
 }
-
-source_suffix = [".md", ".ipynb"]
-master_doc = "index"
-default_role = "literal"
-
-# Generate the API documentation
-autosummary_generate = True
-autodoc_member_order = "bysource"
-napoleon_google_docstring = True
-napoleon_numpy_docstring = False
-napoleon_include_init_with_doc = False
-napoleon_use_rtype = True  # having a separate entry generally helps readability
-napoleon_use_param = True
-todo_include_todos = False
-nitpicky = True  # report broken links
-
-
-def setup(app: Sphinx):
-    # Don’t allow broken links. DO NOT CHANGE THIS LINE, fix problems instead.
-    app.warningiserror = False
-    app.add_css_file("custom.css")
