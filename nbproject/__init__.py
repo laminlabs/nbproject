60a6b90ef7a05167b7098ee09d47b59044e24123
Koncopd <koncopd@gmail.com>
2022-05-17 10:41:17 +0200

    try fixing flit build

--- nbproject/__init__.py
+++ nbproject/__init__.py
@@ -1,4 +1,5 @@
 """nbproject: Manage your notebooks."""
+import traceback
 import sys
 from types import ModuleType
 
@@ -14,3 +15,14 @@ class LazyVersion(ModuleType):
 
 
 sys.modules[__name__].__class__ = LazyVersion
+
+
+def within_flit():
+    for frame in traceback.extract_stack():
+        if frame.name == "get_docstring_and_version_via_import":
+            return True
+    return False
+
+
+if within_flit():
+    sys.modules[__name__].__dict__["__version__"] = sys.modules[__name__].__version__
