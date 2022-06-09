d2fc38abaa49d2d102e10179bee65914855e20f7
Alex Wolf <f.alexander.wolf@gmail.com>
2022-06-09 09:07:02 +0200

    :adhesive_bandage: Fix title formatting (#79)
    * :adhesive_bandage: Fix title formatting
    
    * :bug: fix bug

--- nbproject/_meta.py
+++ nbproject/_meta.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+from typing import Mapping, Union
 
 import orjson
 
@@ -8,6 +9,23 @@ Meta = namedtuple("Meta", ["id", "time_init", "title"])
 _filepath = ""
 
 
+def get_title(nb: Mapping) -> Union[str, None]:
+    title_error = (
+        "Warning: No title! Please update & save your notebook so that it has a"
+        " markdown cell with the title: # My title"
+    )
+    if nb["cells"][0]["cell_type"] != "markdown":
+        print(title_error)
+        title = None
+    else:
+        title = nb["cells"][0]["source"][0]
+        if not title.startswith("# "):
+            print(title_error)
+        else:
+            title = title.lstrip("#").strip(" .")
+    return title
+
+
 def _load_meta():
     if _filepath == "":
         return Meta(id=None, time_init=None, title=None)
@@ -15,21 +33,8 @@ def _load_meta():
         with open(_filepath, "rb") as f:
             nb = orjson.loads(f.read())
 
-    title_error = (
-        "Warning: No title! Please update & save your notebook so that it has a"
-        " markdown cell with the title: # My title"
-    )
-    if nb["cells"][0]["cell_type"] != "markdown":
-        print(title_error)
-        print("hello")
-        title = None
-    else:
-        title = nb["cells"][0]["source"][0]
-        if not title.startswith("# "):
-            print(title_error)
-
     return Meta(
         id=nb["metadata"]["nbproject"]["id"],
         time_init=nb["metadata"]["nbproject"]["time_init"],
-        title=title,
+        title=get_title(nb),
     )
