94493a7c5d1550a0d5f4b1abbd83e9f2639d95f6
Koncopd <koncopd@gmail.com>
2022-06-09 13:36:40 +0200

    no writes

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -118,7 +118,7 @@ def test_notebooks():
     sleep(5)
 
     try:
-        execute_notebooks(nb_folder, write=True)
+        execute_notebooks(nb_folder, write=False)
     except Exception as e:
         kill_process(p)
         raise e
