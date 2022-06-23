8f004feaa6e6a7fd6b281aab258da64ed21b9907
Alex Wolf <f.alexander.wolf@gmail.com>
2022-06-23 18:21:36 +0200

    Add documentation to dev module

--- nbproject/dev/__init__.py
+++ nbproject/dev/__init__.py
@@ -1,7 +1,13 @@
-"""Developer functions."""
+"""Developer functions.
+
+.. autoclass:: Notebook
+   :members:
+   :undoc-members:
+
+"""
 from . import _test as test
 from ._dependency import infer_dependencies
 from ._initialize import initialize_metadata
 from ._integrity import check_integrity
 from ._jupyter_communicate import notebook_path
-from ._notebook import read_notebook, write_notebook
+from ._notebook import read_notebook, write_notebook, Notebook
