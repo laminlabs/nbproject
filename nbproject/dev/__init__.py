062a1fad7e2b5f97d8c19aa3b8f396233dd5581f
Koncopd <koncopd@gmail.com>
2022-06-23 18:33:03 +0200

    format

--- nbproject/dev/__init__.py
+++ nbproject/dev/__init__.py
@@ -10,4 +10,4 @@ from ._dependency import infer_dependencies
 from ._initialize import initialize_metadata
 from ._integrity import check_integrity
 from ._jupyter_communicate import notebook_path
-from ._notebook import read_notebook, write_notebook, Notebook
+from ._notebook import Notebook, read_notebook, write_notebook
