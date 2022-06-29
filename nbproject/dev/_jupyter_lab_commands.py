69153c2ef342880b2324f700613688a7338f1990
Alex Wolf <f.alexander.wolf@gmail.com>
2022-06-29 23:11:05 +0200

    :bulb: Add comments on better UX in Jupyter Lab

--- nbproject/dev/_jupyter_lab_commands.py
+++ nbproject/dev/_jupyter_lab_commands.py
@@ -19,5 +19,18 @@ def _restart_notebook():
     if _app is None:
         _app = JupyterFrontEnd()
 
-    _app.commands.execute("docmanager:reload")
+    _app.commands.execute("docmanager:reload")  # reload notebook from disk
     _app.commands.execute("notebook:restart-and-run-to-selected")
+    # It'd be nice to execute more cells here, but that's impossible
+    # as the kernel is dead at this point
+    #
+    # "notebook:restart-run-all" is also no alternative, as that will run
+    # the entire notebook, which might be undesirable
+    # also in combination with something like the below in `Header()` seems hacky
+    # as it prints a big "Keyboard Interrupt"
+    # if (
+    #     time_run - datetime.fromisoformat(nb.metadata["nbproject"]["time_init"])
+    # ) < 2:
+    # from ipylab import JupyterFrontEnd
+    # app = JupyterFrontEnd()
+    # app.commands.execute('notebook:interrupt-kernel')
