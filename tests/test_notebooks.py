d95fb7b4a67df676eed25734ab8f0f19871fa0e1
Koncopd <koncopd@gmail.com>
2022-06-29 20:24:34 +0200

    fix tests

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -5,6 +5,8 @@ from nbproject.dev import test
 
 def test_notebooks():
     # assuming this is in the tests folder
-    nb_folder = Path(__file__).parents[1] / "docs/"
+    docs_folder = Path(__file__).parents[1] / "docs/"
 
-    test.execute_notebooks(nb_folder, write=True)
+    for check_folder in docs_folder.glob("./*"):
+        if check_folder.is_dir():
+            test.execute_notebooks(check_folder, write=True)
