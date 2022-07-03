f207cee9bef435468de57bbf9ee8453a8df086ed
Koncopd <koncopd@gmail.com>
2022-07-03 16:27:16 +0200

    go through all folders and subfolders

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+from nbproject._logger import logger
 from nbproject.dev import test
 
 
@@ -7,6 +8,6 @@ def test_notebooks():
     # assuming this is in the tests folder
     docs_folder = Path(__file__).parents[1] / "docs/"
 
-    for check_folder in docs_folder.glob("./*"):
-        if check_folder.is_dir():
-            test.execute_notebooks(check_folder, write=True)
+    for check_folder in docs_folder.glob("./**"):
+        logger.debug(f"\n{check_folder}")
+        test.execute_notebooks(check_folder, write=True)
