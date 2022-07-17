0001a1ef8bae76eea325fb678b7ac949624c4962
Alex Wolf <f.alexander.wolf@gmail.com>
2022-07-17 19:24:28 +0200

    :green_heart: Fix noxfile

--- noxfile.py
+++ noxfile.py
@@ -12,7 +12,7 @@ def lint(session: nox.Session) -> None:
     session.run("pre-commit", "run", "--all-files")
 
 
-@nox.session(python=["3.7", "3.9"])
+@nox.session(python=["3.7", "3.8", "3.9", "3.10"])
 def build(session):
     session.install(".[dev,test]")
     session.run(
