7a21bc5a9ebba44dee7ae174d58961437ada4b23
Sunny Sun <38218185+sunnyosun@users.noreply.github.com>
2022-07-18 23:53:45 +0800

    :lipstick: Logging with icons

--- nbproject/_logger.py
+++ nbproject/_logger.py
@@ -4,7 +4,8 @@ from loguru import logger
 
 default_handler = dict(
     sink=sys.stdout,
-    format="{message}",
+    format="{level.icon} {message}",
 )
 
 logger.configure(handlers=[default_handler])
+logger.level("WARNING", icon="🔶")
