f74f220367e0be810ac771e3cef2de505168e186
Alex Wolf <f.alexander.wolf@gmail.com>
2022-07-19 22:12:40 +0200

    :recycle: Refactor and test set_version

--- nbproject/dev/__init__.py
+++ nbproject/dev/__init__.py
@@ -16,6 +16,7 @@ Functionality
 .. autofunction:: infer_pypackages_from_nb
 .. autofunction:: infer_pypackages_from_file
 .. autofunction:: check_consecutiveness
+.. autofunction:: set_version
 
 Helpers
 -------
@@ -35,3 +36,4 @@ from ._meta_live import MetaLive
 from ._meta_store import MetaContainer, MetaStore
 from ._notebook import Notebook, read_notebook, write_notebook
 from ._pypackage import infer_pypackages_from_file, infer_pypackages_from_nb
+from ._set_version import set_version
---
+++ nbproject/dev/_set_version.py
@@ -0,0 +1,33 @@
+from typing import Union
+
+from .._logger import logger
+from .._meta import meta
+
+
+def set_version(
+    version: Union[str, None] = None, stored_version: Union[str, None] = None
+):
+    """(Auto-) set version.
+
+    If `None`, bumps the version from "draft" to "1", from "1" to "2", etc.
+    Otherwise sets the version to the passed version.
+
+    Args:
+        version: Pass version string.
+        stored_version: Mock stored version for testing purposes.
+    """
+    if stored_version is None:
+        stored_version = meta.store.version
+
+    if version is not None:
+        meta.store.version = version
+    else:
+        try:
+            if stored_version == "draft":
+                version = "1"
+            else:
+                version = str(int(stored_version) + 1)  # increment version by 1
+            meta.store.version = version
+        except ValueError:
+            logger.error("The version cannot be auto-set. Please pass a version.")
+            return "manual-version"
