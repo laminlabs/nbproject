66d50b2a6e595d1ca9f2d0ef493ee7c5e1d96335
Koncopd <koncopd@gmail.com>
2022-08-16 11:00:40 +0200

    do not show js object

--- nbproject/dev/_classic_nb_commands.py
+++ nbproject/dev/_classic_nb_commands.py
@@ -5,23 +5,23 @@ from .._logger import logger
 
 def _save_notebook():
     try:
-        from IPython.display import Javascript, display
+        from IPython.display import Javascript, display_javascript
     except ModuleNotFoundError:
         logger.warning("Can not import from IPython.")
         return None
 
     js = Javascript("IPython.notebook.save_notebook()")
-    display(js)
+    display_javascript(js)
 
     sleep(1)
 
 
 def _reload_notebook():
     try:
-        from IPython.display import Javascript, display
+        from IPython.display import Javascript, display_javascript
     except ModuleNotFoundError:
         logger.warning("Can not import from IPython.")
         return None
 
     js = Javascript("IPython.notebook.load_notebook(IPython.notebook.notebook_path)")
-    display(js)
+    display_javascript(js)
