7e9c751233dbab2423bf3a961c51850379c9358f
Alex Wolf <f.alexander.wolf@gmail.com>
2022-08-29 10:10:19 +0200

    :lipstick: Clean test notebooks

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -5,20 +5,8 @@ from nbproject.dev import test
 
 
 def test_notebooks():
-    # assuming this is in the tests folder
     docs_folder = Path(__file__).parents[1] / "docs/"
 
     for check_folder in docs_folder.glob("./**"):
-        # the following are the typical notebook test paths
-        # if not str(check_folder).endswith(("guides", "tutorials")):
-        #     continue
-        # usually we specify them in pyproject.toml via
-        # [tool.pytest.ini_options]
-        # testpaths = [
-        #     "tests",
-        #     "docs/tutorials",
-        #     "docs/guides",
-        # ]
-        # here, we need to specify them manually
         logger.debug(f"\n{check_folder}")
         test.execute_notebooks(check_folder, write=True)
