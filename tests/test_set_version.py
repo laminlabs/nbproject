23a9ad1f50a3e6f924ff7435ac03d0d36c468c28
Alex Wolf <f.alexander.wolf@gmail.com>
2022-11-12 19:44:57 +0100

    :green_heart: Fix test

--- tests/test_set_version.py
+++ tests/test_set_version.py
@@ -3,7 +3,7 @@ from nbproject.dev._set_version import set_version
 
 def test_set_version():
     # all remaining lines are covered in notebooks
-    assert set_version(None, "1.2") == "manual-version"
-    assert set_version(None, "0") == "1"
-    assert set_version(None, "1") == "2"
+    assert set_version(None, "1.2") == "1.2"
+    assert set_version(None, "0") == "0"
+    assert set_version(None, "1") == "1"
     assert set_version("1.2.3", "0") == "1.2.3"
