759bf89c29b96328c5889bfda3109ffab377b9fc
Alex Wolf <f.alexander.wolf@gmail.com>
2022-11-12 19:50:16 +0100

    🚸 Do no longer auto-bump `version` upon `publish()` (#244)
    * :children_crossing: Do not auto-bump version
    
    * :green_heart: Fix test

--- nbproject/dev/_set_version.py
+++ nbproject/dev/_set_version.py
@@ -1,6 +1,5 @@
 from typing import Union
 
-from .._logger import logger
 from .._meta import meta
 
 
@@ -9,11 +8,11 @@ def set_version(
 ):
     """(Auto-) set version.
 
-    If `None`, bumps the version from "0" to "1", from "1" to "2", etc.
+    If `version` is `None`, returns the stored version.
     Otherwise sets the version to the passed version.
 
     Args:
-        version: Pass version string.
+        version: Version string.
         stored_version: Mock stored version for testing purposes.
     """
     if stored_version is None:
@@ -22,12 +21,4 @@ def set_version(
     if version is not None:
         return version
     else:
-        try:
-            if stored_version == "0":
-                version = "1"
-            else:
-                version = str(int(stored_version) + 1)  # increment version by 1
-            return version
-        except ValueError:
-            logger.error("The version cannot be auto-set. Please pass a version.")
-            return "manual-version"
+        return stored_version
