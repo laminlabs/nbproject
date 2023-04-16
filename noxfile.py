7455f1a248687c412a12d646ad7e5c60ccb864c1
Alex Wolf <f.alexander.wolf@gmail.com>
2023-04-16 07:09:29 +0200

    :green_heart: Log in

--- noxfile.py
+++ noxfile.py
@@ -1,6 +1,6 @@
 import nox
 from laminci import move_built_docs_to_docs_slash_project_slug, upload_docs_artifact
-from laminci.nox import build_docs, run_pre_commit, run_pytest
+from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
 
 nox.options.reuse_existing_virtualenvs = True
 
@@ -12,6 +12,7 @@ def lint(session: nox.Session) -> None:
 
 @nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
 def build(session):
+    login_testuser1(session)
     session.install(".[dev,test]")
     run_pytest(session)
     build_docs(session)
