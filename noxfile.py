48312cd9b9c7d851b343cc39629c8ba2bbbdc28e
Alex Wolf <f.alexander.wolf@gmail.com>
2023-06-15 21:02:34 +0200

    💚 Simplify CI

--- noxfile.py
+++ noxfile.py
@@ -2,7 +2,7 @@ import nox
 from laminci import move_built_docs_to_docs_slash_project_slug, upload_docs_artifact
 from laminci.nox import build_docs, login_testuser1, run_pre_commit, run_pytest
 
-nox.options.reuse_existing_virtualenvs = True
+nox.options.default_backend = "none"
 
 
 @nox.session
@@ -10,7 +10,7 @@ def lint(session: nox.Session) -> None:
     run_pre_commit(session)
 
 
-@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
+@nox.session
 def build(session):
     login_testuser1(session)
     session.install(".[dev,test]")
