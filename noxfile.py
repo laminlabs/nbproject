ea86a499fae4eec59b2f85fba93fae39ffdf207e
Alex Wolf <f.alexander.wolf@gmail.com>
2024-05-20 11:44:24 +0200

    👷 Move docs to cloudflare

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,4 @@
 import nox
-from laminci import move_built_docs_to_docs_slash_project_slug
 from laminci.nox import build_docs, run_pre_commit, run_pytest
 
 nox.options.default_backend = "none"
@@ -15,4 +14,3 @@ def build(session):
     session.run(*"pip install .[dev]".split())
     run_pytest(session)
     build_docs(session)
-    move_built_docs_to_docs_slash_project_slug()
